1114a8b5e8ad2eafed951225513ff1758b34873c
xbb73 <xbb73@outlook.com>
2023-10-06 11:04:25 -0400

    CHANGE TWO

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/xavier/i);
+  const linkElement = screen.getByText(/best test ever/i);
   expect(linkElement).toBeInTheDocument();
 });
